d1c2de5b58b970704d71bfccc1082047002df058
Solomoni Railoa <90390564+solomonirailoa@users.noreply.github.com>
2023-06-25 23:53:20 +1200

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,3 +1,4 @@
+import Button from 'react-bootstrap/Button';
 import MenuNavBar from "../partials/MenuNavBar";
 import Footer from "../partials/Footer";
 import PageTitle from "../partials/PageTitle";
@@ -17,65 +18,95 @@ function Products() {
 
                 {/* Responsive grid below, rowHome */}
                 <div className="rowHome">
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Grotto Escape</h1>
                         <p>Product: Game</p>
-                        <p>Author: David Dolejsi</p>
+                        <p>Developer: David Dolejsi</p>
                         <p>Genre: Action, Run</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Play game</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Chicken Shooter</h1>
                         <p>Product: Game</p>
-                        <p>Author: Tejas Shreemali</p>
+                        <p>Developer: Tejas Shreemali</p>
                         <p>Genre: Shooter, Action</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Play game</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Rocket 69</h1>
                         <p>Product: Game</p>
-                        <p>Author: David Dolejsi</p>
+                        <p>Developer: David Dolejsi</p>
                         <p>Genre: Fly, Flappy, Rocket, Space</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Play game</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Promote my SL Code</h1>
                         <p>Product: App</p>
-                        <p>Author: S.I.A</p>
-                        <p>Genre: Sololearn Intelligence Agency</p>
+                        <p>Developer: S.I.A</p>
+                        <p>Genre: Sololearn Intelligence Agency, Tools, Hack, Boost</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Use this app</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>C' ynth</h1>
                         <p>Product: App</p>
-                        <p>Author: ChillPill</p>
+                        <p>Developer: ChillPill</p>
                         <p>Genre: C-sound, C, Synth, Music</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Use this app</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Turtle Game</h1>
                         <p>Product: Game</p>
-                        <p>Author: Cosmin Turtureanu</p>
+                        <p>Developer: Cosmin Turtureanu</p>
                         <p>Genre: Life, Reality, Ant</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Play game</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>WeblerOverflow</h1>
                         <p>Product: App</p>
-                        <p>Author: Webler</p>
+                        <p>Developer: Webler</p>
                         <p>Genre: Q&A Discussion, Forum</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Join Community</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Weblerlearn</h1>
                         <p>Product: App</p>
-                        <p>Author: Webler</p>
+                        <p>Developer: Webler</p>
                         <p>Genre: Learning Platform, Courses, Lessons</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Enroll to our courses</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>WeblerHub</h1>
                         <p>Product: App</p>
-                        <p>Author: Webler</p>
-                        <p>Genre: Social network, chat, media</p>
+                        <p>Developer: Webler</p>
+                        <p>Genre: Social network, Chat, Media</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Meet and Chat</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-7">
                         <h1>Merch</h1>
                         <p>Product: Goods & Services</p>
                         <p>Distributor: Webler</p>
                         <p>Genre: Merchandise</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, "to add a short description of this product here" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <Button variant="primary">Buy our merch</Button><span style={{float:"right", fontSize:"12px"}}> 96 likes - 69 comments - 403 views</span>
                     </div>
                 </div>
 
